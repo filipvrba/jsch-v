ca47bea9e8de778b133dd9bb556f0d8eaf479b83
Filip Vrba <filipvrba@pm.me>
2024-02-14 00:21:57 +0100

    fix print events

--- src/main.v
+++ src/main.v
@@ -6,16 +6,15 @@ import os
 
 fn main() {
 	args := unsafe { os.args }
-	app_name := os.file_name(args[0]).replace_once('\.$', '')
 
 	if args.len > 3 {
 		mut json_parser := jp.JsonParser{ path: args[1] }
 		json_parser.set(args[2], args[3])
-		rb.Event{'set', app_name}
+		rb.Event{name: 'set'}
 			.println("${args[2]}: ${args[3]}")
 	}
 	else {
-		rb.Event{'error', app_name}
+		rb.Event{name: 'error'}
 			.println('Add an 3 arguments (file, key, value)!')
 	}
 }
